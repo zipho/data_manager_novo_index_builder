27ce48fc780c08d14a2f46c9174bb798976e521f
zipho <zipho.m@gmail.com>
2016-04-18 15:13:56 +0200

    changes

--- data_manager/novocraft_index_builder.py
+++ data_manager/novocraft_index_builder.py
@@ -4,7 +4,6 @@
 from __future__ import print_function
 import os
 import sys
-import urllib2
 import logging
 import argparse
 import shlex
@@ -16,13 +15,14 @@ from json import loads, dumps
 
 DEFAULT_DATA_TABLE_NAME = "novocraft_index"
 
-def get_dbkey_id_name(params, dbkey_description=None):
+def get_dbkey_id_name(params):
     # TODO: ensure sequence_id is unique and does not already appear in location file
     sequence_id = params['param_dict']['sequence_id']
     sequence_name = params['param_dict']['sequence_name']
-    if not sequence_name:
-        sequence_name = dbkey_description
-    return sequence_id, sequence_name
+    sequence_desc = params['param_dict']['sequence_desc']
+    if not sequence_desc:
+        sequence_desc = sequence_name
+    return sequence_id, sequence_name, sequence_desc
 
 def _make_novocraft_index(data_manager_dict, fasta_filename, target_directory, sequence_id, sequence_name, data_table_name=DEFAULT_DATA_TABLE_NAME):
     if os.path.exists(target_directory) and not os.path.isdir(target_directory):
@@ -50,25 +50,10 @@ def _add_data_table_entry( data_manager_dict, data_table_name, data_table_entry 
     data_manager_dict['data_tables'][ data_table_name ].append( data_table_entry )
     return data_manager_dict
 
-def download_from_url( data_manager_dict, params, target_directory, sequence_id, sequence_name, data_table_name=DEFAULT_DATA_TABLE_NAME ):
-    # TODO: we should automatically do decompression here
-    urls = filter(bool, map(lambda x: x.strip(), params['param_dict']['reference_source']['user_url'].split('\n')))
-    fasta_reader = [urllib2.urlopen(url) for url in urls]
-
-    _make_novocraft_index(data_manager_dict, fasta_reader, target_directory, sequence_id, sequence_name, data_table_name)
-
-def download_from_history( data_manager_dict, params, target_directory,  sequence_id, sequence_name, data_table_name=DEFAULT_DATA_TABLE_NAME ):
-    # TODO: allow multiple FASTA input files
-    input_filename = params['param_dict']['reference_source']['input_fasta']
-
-    _make_novocraft_index(data_manager_dict, input_filename, target_directory, sequence_id, sequence_name, data_table_name )
-
-REFERENCE_SOURCE_TO_DOWNLOAD = dict(url=download_from_url, history=download_from_history)
-
 def main():
     parser = argparse.ArgumentParser(description="Generate Novo-craft genome index and JSON describing this")
     parser.add_argument('output_filename')
-    parser.add_argument('--dbkey_description')
+    parser.add_argument('--input_filename')
     parser.add_argument('--data_table_name', default='novocraft_index')
     args = parser.parse_args()
 
@@ -79,11 +64,10 @@ def main():
     os.makedirs(target_directory)
     data_manager_dict = {}
 
-    sequence_id, sequence_name = get_dbkey_id_name(params, dbkey_description=args.dbkey_description)
+    sequence_id, sequence_name, sequence_desc = get_dbkey_id_name(params)
 
-    # Fetch the FASTA
-    REFERENCE_SOURCE_TO_DOWNLOAD[params['param_dict']['reference_source']['reference_source_selector']]\
-        (data_manager_dict, params, target_directory, sequence_id, sequence_name, data_table_name=args.data_table_name or DEFAULT_DATA_TABLE_NAME )
+    #Make novocraft index
+    _make_novocraft_index(data_manager_dict, args.input_filename, target_directory, sequence_id, sequence_name, args.data_table_name or DEFAULT_DATA_TABLE_NAME )
 
     open(filename, 'wb').write(dumps( data_manager_dict ))
 
